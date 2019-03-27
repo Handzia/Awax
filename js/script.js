08a293a69a52104269dc7d5e23b4b914398055ed
Anna Szmyga <anna.szmyga@o2.pl>
2019-03-27 16:33:41 +0100

    Add responsive styles and mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+'use strict'
+
+// MOBILE MENU
+
+
+function hideMenu() {
+    document.querySelector('.menu').classList.toggle('hidden');
+}
+var hamburgerButton = document.querySelector('.hamburger');
+hamburgerButton.addEventListener('click', hideMenu);
+
+
+// CAROUSEL
+
 var flkty = new Flickity( '.clients-carousel', {
   wrapAround: true,
   autoPlay: true
 });
+
