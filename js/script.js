8ef3455566b5c86c50963f609a7fbc8f31e465d2
Anna Szmyga <anna.szmyga@o2.pl>
2019-04-03 16:25:51 +0200

    Add modals

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 
 
 function hideMenu() {
-    document.querySelector('.menu').classList.toggle('hidden');
+  document.querySelector('.menu').classList.toggle('hidden');
 }
 const hamburgerButton = document.querySelector('.hamburger');
 hamburgerButton.addEventListener('click', hideMenu);
 
 
-// CAROUSEL
+// CAROUSELS
 
 const flkty1 = new Flickity( '.jumbotron-carousel', {
   wrapAround: true,
@@ -24,15 +24,58 @@ const flkty2 = new Flickity( '.clients-carousel', {
 });
 
 function setCarouselHeight() {
-    let height = document.querySelector('.jumbotron-content').offsetHeight + 50;
-    document.querySelector('.flickity-viewport').style.height = height + 'px';
-    let slides = document.querySelectorAll('.jumbotron-carousel-cell');
-    for ( let i = 0; i < slides.length; i++) {
-       slides[i].style.height = height + 'px';
-       flkty1.cells[i].size.height = height;
-    }
+  let height = document.querySelector('.jumbotron-content').offsetHeight + 50;
+  document.querySelector('.flickity-viewport').style.height = height + 'px';
+  let slides = document.querySelectorAll('.jumbotron-carousel-cell');
+  for ( let i = 0; i < slides.length; i++) {
+    slides[i].style.height = height + 'px';
+    flkty1.cells[i].size.height = height;
+  }
 }
 
 setCarouselHeight();
 
 window.addEventListener("resize", setCarouselHeight);
+
+
+// MODALS
+
+function showModal(modal) {
+  document.querySelector('.overlay').classList.add('show');
+  document.getElementById(modal).classList.add('show');
+};
+
+function hideModal() {
+  document.querySelector('.overlay').classList.remove('show');
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(function(modal) {modal.classList.remove('show')});
+
+};
+
+const telephoneButton = document.querySelector('#telephone-button');
+const callBackButton = document.querySelector('#call-back-button');
+const subscribeButton = document.querySelector('#subscribe-button');
+
+telephoneButton.addEventListener('click', function(e) {
+  showModal('call-modal');
+});
+subscribeButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  showModal('thank-you-modal');
+});
+
+
+callBackButton.addEventListener('click', hideModal);
+
+document.querySelector('.overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    hideModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    hideModal();
+  }
+});
+
